6a834ab6a4fd195fcf01cf83f85b8159dfd99625
dhubmann <daniel.hubmann@gmx.at>
2023-07-07 10:39:29 +0200

    Change implementation of Two Sum
    I changed the solution for the Two Sum example to a much more concise
    version. It now contains only one checksum() function that compares the
    checksum of two numbers in the array against a given target.
    
    The function has a runtime of O(n(U+00B2)) though making it very inefficient.
    A more efficient solution should be implemented.

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,12 +1,11 @@
 /*
- * 
- * 
- * 
+ * TwoSum
+ * Represents Two Sum Example
+ * Author: Daniel Hubmann
+ * Last Change: 07.07.2023
  */
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Random;
 
 /*
  * Given an array of integers 'nums' and an integer 'target', return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
@@ -19,84 +18,29 @@ public class TwoSum {
 		int target = 9;
 
 		TwoSum twoSum = new TwoSum();
-		pairs = twoSum.pairs(nums);
 
-		// Print all possible pairs
-		for (HashSet<Integer> pair : pairs) {
-			for (Integer i : pair) {
-				System.out.print(i + ", ");
-			}
-			System.out.println();
-		}
-
-		targetPair = twoSum.checksum(pairs, target);
-
-		// Print result
-		System.out.print("Target Pair: ");
-		for (Integer i : targetPair) {
-			System.out.print(i + " ");
-		}
-		System.out.println();
-
-		// Print indices
-		indices = twoSum.indices(targetPair, nums);
-		System.out.println(indices.toString());
-
+		indices = twoSum.checksum(nums, target);
+		System.out.println(indices);
+		
 	}
-
-	static HashSet<HashSet<Integer>> pairs;
-	static HashSet<Integer> targetPair;
-	static ArrayList<Integer> indices;
+	
+	static HashSet<Integer> indices;
 
 	/*
-	 * Creates every possible pair of numbers from a given array of numbers
+	 * Method has O(n²) - find more efficient solution
 	 */
-	public HashSet<HashSet<Integer>> pairs(int[] nums) {
+	public HashSet<Integer> checksum(int[] nums, int target) {
 
-		HashSet<HashSet<Integer>> pairs = new HashSet<HashSet<Integer>>();
-		for (int i = 0; i < 1000; i++) {
-			HashSet<Integer> pair = new HashSet<Integer>();
-			while (pair.size() != 2) {
-				Random random = new Random();
-				int index = random.nextInt(4);
-				pair.add(nums[index]);
-			}
-			pairs.add(pair);
-		}
-		return pairs;
-	}
+		HashSet<Integer> indices = new HashSet<Integer>();
 
-	/*
-	 * Returns a pair of integers if their checksum equals a given target
-	 */
-	public HashSet<Integer> checksum(HashSet<HashSet<Integer>> pairs, int target) {
-
-		for (HashSet<Integer> pair : pairs) {
-			int checksum = 0;
-			for (Integer i : pair) {
-				checksum += i;
-				if (rightTarget(checksum, target)) {
-					return pair;
-				}
-			}
-		}
-		return null;
-	}
-
-	private boolean rightTarget(int checksum, int target) {
-		return checksum == target ? true : false;
-	}
-
-	public ArrayList<Integer> indices(HashSet<Integer> pair, int[] nums) {
-
-		ArrayList<Integer> indices = new ArrayList<Integer>();
-		for (Integer num : pair) {
-			for (int i = 0; i < nums.length; i++) {
-				if (num == nums[i]) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (nums[i] != nums[j] && (nums[i] + nums[j]) == target) {
 					indices.add(i);
 				}
 			}
 		}
+
 		return indices;
 	}
 
