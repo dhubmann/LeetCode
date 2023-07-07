c0edd1a274f7e3c42e3d000f7f82a647fa296713
dhubmann <daniel.hubmann@gmx.at>
2023-07-07 16:28:51 +0200

    Create & implement ContainsDuplicate example
    Added two possible implementations of the Contains Duplicate example.
    
    One solution compares each number against a set of visited numbers
    (while iterating over the given array) - with a runtime complexity of
    O(n).
    
    The other solution compares each number with every number (except the
    same number itself) using two nested for loops - therefore with a less effecient runtime complexity of
    O(n^2).

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,69 @@
+/*
+ * ContainsDuplicate
+ * Represents Contains Duplicate Example
+ * Author: Daniel Hubmann
+ * Last Change: 07.07.2023
+ */
+
+import java.util.HashSet;
+
+/**
+ * Given an integer array 'nums', return true if any value appears at least
+ * twice in the array, and return false if every element is distinct.
+ * 
+ * @author Daniel Hubmann
+ *
+ */
+public class ContainsDuplicate {
+
+	public static void main(String[] args) {
+
+		int[] _1231 = { 1, 2, 3, 1 }; // result: true
+		int[] _1234 = { 1, 2, 3, 4 }; // result: false
+
+		// Solution 1
+		ContainsDuplicate cd = new ContainsDuplicate();
+		boolean result = cd.containsDuplicate(_1231);
+		System.out.println(result);
+
+		// Solution 2
+		boolean resultNestedForLoops = cd.containsDuplicateNestedForLoops(_1231);
+		System.out.println(resultNestedForLoops);
+
+	}
+
+	/*
+	 * Comparing each number against a set containing visited numbers - runtime
+	 * complexity of O(n).
+	 */
+	public boolean containsDuplicate(int[] nums) {
+
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (set.contains(nums[i])) {
+				return true;
+			} else {
+				set.add(nums[i]);
+			}
+		}
+
+		return false;
+	}
+
+	/*
+	 * Comparing each number with every number - runtime complexity of O(n²).
+	 */
+	public boolean containsDuplicateNestedForLoops(int[] nums) {
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (i != j && nums[i] == nums[j]) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+}
