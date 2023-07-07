5d3a9d3033302c4d27d0ee12c0dcca6c09a78d28
dhubmann <daniel.hubmann@gmx.at>
2023-07-07 11:38:31 +0200

    "Add alternative solution to Two Sum"
    Added a second, more efficient solution to the Two Sum example.
    
    We use an HashMap to map each number of the array and its index to check
    for a valid combination of numbers while iterating over the array.
    
    Since we only need to iterate over the array once, runtime is at O(n).

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -5,10 +5,14 @@
  * Last Change: 07.07.2023
  */
 
+import java.util.HashMap;
 import java.util.HashSet;
 
-/*
- * Given an array of integers 'nums' and an integer 'target', return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+/**
+ * Given an array of integers 'nums' and an integer 'target', return indices of
+ * the two numbers such that they add up to target. You may assume that each
+ * input would have exactly one solution, and you may not use the same element
+ * twice. You can return the answer in any order.
  */
 public class TwoSum {
 
@@ -19,12 +23,50 @@ public class TwoSum {
 
 		TwoSum twoSum = new TwoSum();
 
-		indices = twoSum.checksum(nums, target);
-		System.out.println(indices);
-		
+		// Solution 1
+		indicesSet = twoSum.checksum(nums, target);
+		System.out.println(indicesSet);
+
+		// Solution 2
+		indicesArray = twoSum.check(nums, target);
+		twoSum.printArray(indicesArray);
+
 	}
-	
-	static HashSet<Integer> indices;
+
+	static HashSet<Integer> indicesSet;
+	static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	static int[] indicesArray = new int[2];
+
+	/*
+	 * More efficient solution - O(n)
+	 */
+	public int[] check(int[] nums, int target) {
+
+		int[] indices = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target - nums[i])) {
+				indices[0] = map.get(target - nums[i]);
+				indices[1] = i;
+			} else {
+				map.put(nums[i], i);
+			}
+		}
+
+		return indices;
+
+	}
+
+	public void printArray(int[] array) {
+		System.out.print("[");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if (i != array.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("]");
+	}
 
 	/*
 	 * Method has O(n²) - find more efficient solution
